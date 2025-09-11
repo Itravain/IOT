67f02a304dde74389ef8aa567a35471446f2e557
manoel-serafim <manoel.serafim@proton.me>
2025-09-11 18:23:48 -0300

    (feat) Initial/Final? commit

--- src/ativ_menor_1/main.cpp
+++ src/ativ_menor_1/main.cpp
@@ -1,77 +1,133 @@
-#include <Arduino.h>
+#define IDLE 0
+#define CLEAN 1
+#define DOCK 2
+#define CHRG 3
 
-typedef enum {IDLE, CLEAN, DOCK, CHRG, WAIT} Estado;
+#define LED1_PIN 2
+#define LED2_PIN 3
 
-unsigned long timer;
+int estadoAtual = IDLE;
+unsigned long timerEstado;
+unsigned long timerLED1;
+unsigned long timerLED2;
+boolean estadoLED1 = false;
+boolean estadoLED2 = false;
+
+#define TEMPO_TRANSICAO 5000
+#define INTERVALO_LED1 100
+#define INTERVALO_LED2 50
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(2, OUTPUT);
-  pinMode(0, OUTPUT);
+  Serial.begin(9600);
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+  
+  timerEstado = millis();
+  timerLED1 = millis();
+  timerLED2 = millis();
+  
+  Serial.println("Roomba Inicializado!");
+  Serial.println("Comandos: 'a' = Clean, 'b' = Dock");
+  Serial.println("Estado atual: IDLE");
 }
 
 void loop() {
-  static Estado estados = IDLE;
-  static char command;
-  switch (estados)
-  {
-  case IDLE:
-      if(Serial.available()){
-        if(Serial.read() == 'a'){
-          estados = CLEAN;
-          digitalWrite(2, 1);
-          digitalWrite(0, 0);
-          timer = millis();
-        }
-      }
-      digitalWrite(2, 0);
-      digitalWrite(0, 0);
-    break;
-  case CLEAN:
-    if(millis() - timer >= 2000){
-      estados = DOCK;
-      digitalWrite(2, 0);
-      digitalWrite(0, 1);
-      timer = millis();
-    }
-    else if(Serial.available()){
-      if(Serial.read() == 'b'){
-        estados = DOCK;
-        digitalWrite(2, 0);
-        digitalWrite(0, 1);
-        timer = millis();
-      }
-    }
-    
-    break;
-    case DOCK:
-    if(millis() - timer >= 2000){
-        estados = CHRG;
-        digitalWrite(2, 1);
-        digitalWrite(0, 1);
-        timer = millis();
-      }
-          
-    break;
-  case CHRG:
-    if(millis() - timer >= 2000){
-      estados = IDLE;
-      digitalWrite(2, 0);
-      digitalWrite(0, 1);
-      timer = millis();
-    }
-    else if(Serial.available()){
-      if(Serial.read() == 'a'){
-        estados = CLEAN;
-        digitalWrite(2, 0);
-        digitalWrite(0, 1);
-        timer = millis();
-      }
-    }
-    
-    break;
+  char comando = lerComando();
   
-  default:
-    break;
+  switch (estadoAtual) {
+    case IDLE:
+      idle();
+      if (comando == 'a') {
+        mudarEstado(CLEAN);
+      }
+      break;
+      
+    case CLEAN:
+      cleaning();
+      if (comando == 'b') {
+        mudarEstado(DOCK);
+      } else if (millis() - timerEstado >= TEMPO_TRANSICAO) {
+        mudarEstado(DOCK);
+      }
+      break;
+      
+    case DOCK:
+      docking();
+      if (millis() - timerEstado >= TEMPO_TRANSICAO) {
+        mudarEstado(CHRG);
+      }
+      break;
+      
+    case CHRG:
+      charging();
+      if (comando == 'a') {
+        mudarEstado(CLEAN);
+      } else if (millis() - timerEstado >= TEMPO_TRANSICAO) {
+        mudarEstado(IDLE);
+      }
+      break;
+      
+    default:
+      mudarEstado(IDLE);
+      break;
+  }
+}
+
+char lerComando() {
+  if (Serial.available() > 0) {
+    char cmd = Serial.read();
+    delay(10);
+    return cmd;
+  }
+  return 0;
+}
+
+void mudarEstado(int novoEstado) {
+  estadoAtual = novoEstado;
+  timerEstado = millis();
+  
+  Serial.print("Estado alterado para: ");
+  if (novoEstado == IDLE) Serial.println("IDLE");
+  else if (novoEstado == CLEAN) Serial.println("CLEAN");
+  else if (novoEstado == DOCK) Serial.println("DOCK");
+  else if (novoEstado == CHRG) Serial.println("CHRG");
+}
+
+void idle() {
+  digitalWrite(LED1_PIN, LOW);
+  digitalWrite(LED2_PIN, LOW);
+}
+
+void cleaning() {
+  digitalWrite(LED2_PIN, LOW);
+  
+  if (millis() - timerLED1 >= INTERVALO_LED1) {
+    estadoLED1 = !estadoLED1;
+    digitalWrite(LED1_PIN, estadoLED1);
+    timerLED1 = millis();
+  }
+}
+
+void docking() {
+  digitalWrite(LED1_PIN, LOW);
+  
+  if (millis() - timerLED2 >= INTERVALO_LED2) {
+    estadoLED2 = !estadoLED2;
+    digitalWrite(LED2_PIN, estadoLED2);
+    timerLED2 = millis();
+  }
+}
+
+void charging() {
+  if (millis() - timerLED1 >= INTERVALO_LED1) {
+    estadoLED1 = !estadoLED1;
+    digitalWrite(LED1_PIN, estadoLED1);
+    timerLED1 = millis();
+  }
+  
+  if (millis() - timerLED2 >= INTERVALO_LED2) {
+    estadoLED2 = !estadoLED2;
+    digitalWrite(LED2_PIN, estadoLED2);
+    timerLED2 = millis();
   }
 }
